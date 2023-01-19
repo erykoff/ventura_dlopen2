2a1dd4803ae9e86d8d5a174a59f89e8e317a1415
Eli Rykoff <erykoff@stanford.edu>
2023-01-18 16:01:09 -0800

    Try libc++

--- dylibs/compile_dylibs.sh
+++ dylibs/compile_dylibs.sh
@@ -4,5 +4,5 @@ for ((i=0;i<1000;i++));
 do
     echo $i
     clang -c source${i}.c -o source${i}.o
-    clang -dynamiclib -flat_namespace source${i}.o -o source${i}.dylib
+    clang++ -dynamiclib -flat_namespace source${i}.o -o source${i}.dylib -stdlib=libc++
 done
